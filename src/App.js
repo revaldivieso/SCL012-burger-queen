eb076edf02f3e19446fce01cf9a6934de16e094c
consuelowevar <consue_1990@hotmail.com>
2020-03-03 10:32:12 -0300

    creando carpeta de componentes y archivo componente de div menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Component/Menu';
 import firebaseConfig from './firebaseInit.js';
 
 function App() {
@@ -9,8 +10,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Hola bienvenido al inicio del proyecto burger queen
         </p>
+        <h1>Hola mundo</h1>
+        <p><strong>Ejercicio de prueba react</strong></p>
         <a
           className="App-link"
           href="https://reactjs.org"
