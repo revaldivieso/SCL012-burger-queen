f62506ae65eaeb39d19cf0c4ac84ec0c530e4582
consuelowevar <consue_1990@hotmail.com>
2020-03-03 11:30:30 -0300

    agregando archivo de componente MENU

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 class Menu extends Component {
     render() {
         return (
-            <div className='menu'>
-
-            </div>
+            <Container>
+                <Row>
+                    <Col>1 of 3</Col>
+                    <Col xs={6}>2 of 3 (wider)</Col>
+                    <Col>3 of 3</Col>
+                </Row>
+                <Row>
+                    <Col>1 of 3</Col>
+                    <Col xs={5}>2 of 3 (wider)</Col>
+                    <Col>3 of 3</Col>
+                </Row>
+            </Container>
         )
     }
 
 }
 
 export default Menu;
+
+
 
--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,19 +1,19 @@
-// import firebase from 'firebase/app';
-// import 'firebase/firestore';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-// firebaseConfig = {
-//     apiKey: "AIzaSyAmTVJNNsmdyaR9-mv4TDxvhxcm_kXVPio",
-//     authDomain: "burger-queen-5875f.firebaseapp.com",
-//     databaseURL: "https://burger-queen-5875f.firebaseio.com",
-//     projectId: "burger-queen-5875f",
-//     storageBucket: "burger-queen-5875f.appspot.com",
-//     messagingSenderId: "683226958157",
-//     appId: "1:683226958157:web:addba4d22e0ae03d618f04",
-//     measurementId: "G-CKL4V8KWDX"
-//   };
-//   // Initialize Firebase
-//   let db = firebase.initializeApp(firebaseConfig);
+firebaseConfig = {
+    apiKey: "AIzaSyAmTVJNNsmdyaR9-mv4TDxvhxcm_kXVPio",
+    authDomain: "burger-queen-5875f.firebaseapp.com",
+    databaseURL: "https://burger-queen-5875f.firebaseio.com",
+    projectId: "burger-queen-5875f",
+    storageBucket: "burger-queen-5875f.appspot.com",
+    messagingSenderId: "683226958157",
+    appId: "1:683226958157:web:addba4d22e0ae03d618f04",
+    measurementId: "G-CKL4V8KWDX"
+  };
+  // Initialize Firebase
+  let db = firebase.initializeApp(firebaseConfig);
 
-//   export default db;
+  export default db;
 
   
