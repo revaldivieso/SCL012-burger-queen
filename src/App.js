381268a457e49137517caa59d1f15552bdf268c9
consuelowevar <consue_1990@hotmail.com>
2020-03-05 10:41:49 -0300

    maquetacion seccion menu vista mesero

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import './Component/Menu';
-import firebaseConfig from './firebaseInit.js';
+import Menu from './Component/Menu';
+// import firebaseConfig from './firebaseInit.js';
+
+import menu from './data.json';
+console.log(menu);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          Hola bienvenido al inicio del proyecto burger queen
-        </p>
-        <h1>Hola mundo</h1>
-        <p><strong>Ejercicio de prueba react</strong></p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      
+       <Menu>Hola</Menu>
+       
+      
+   
   );
 }
 
--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import './App.css';
-import { Button } from 'react-bootstrap';
+
+// import './App.css';
+ import { Container, Row, Col } from 'react-bootstrap';
+
+
 
 class Menu extends Component {
     render() {
         return (
-            <Container>
-                <Row>
-                    <Col>1 of 3</Col>
-                    <Col xs={6}>2 of 3 (wider)</Col>
-                    <Col>3 of 3</Col>
-                </Row>
-                <Row>
-                    <Col>1 of 3</Col>
-                    <Col xs={5}>2 of 3 (wider)</Col>
-                    <Col>3 of 3</Col>
-                </Row>
-            </Container>
+        <Container className="menu">
+            <h1>Selección Menú</h1>
+            <Row>
+              <Col></Col>
+              <Col></Col>
+              <Col></Col>
+            </Row>
+          </Container>
+
+            
         )
     }
 
 }
 
 export default Menu;
-
-
 
