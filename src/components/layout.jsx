8e18c5745dcbe29f65b10346eb1bd5c82f6be114
reyesvi92@gmail.com <reyesvi92@gmail.com>
2020-03-07 18:56:51 -0300

    creando barra de navegacion

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import SideBar from "./sidebar";
+
 import "./layout.css"
 
 const Layout = (props) => (<div className="layout">
-    <SideBar />
+   
     <div className="content">
         {props.children}
     </div>
