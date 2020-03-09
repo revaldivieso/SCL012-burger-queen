0a2d5c87487c996a5d28e1a37bb4a1b5ea85809a
reyesvi92@gmail.com <reyesvi92@gmail.com>
2020-03-09 18:53:06 -0300

    aplicando estilo al nav bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 import {Navbar, Nav} from 'react-bootstrap';
+import "./Navigation.css"
 
 export class Navigation extends Component {
     render () {
